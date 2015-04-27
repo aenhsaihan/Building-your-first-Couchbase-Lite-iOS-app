2745b826b3e6c4754b7e7d96082637f1d6e03b74
Anar Enhsaihan <anar.enhsaihan@cittacorp.com>
2015-04-27 15:20:13 -0400

    Initial Commit

---
+++ HelloCBL/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloCBL
+//
+//  Created by Anar Enhsaihan on 4/27/15.
+//  Copyright (c) 2015 Couchbase. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HelloCBL/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HelloCBL
+//
+//  Created by Anar Enhsaihan on 4/27/15.
+//  Copyright (c) 2015 Couchbase. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
